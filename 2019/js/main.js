69a4f11b83286c452628d5be18ae219885411553
Rafael Rodriguez <rodrigrv87@gmail.com>
2019-05-11 23:21:58 -0500

    Added functionality for a mobile nav.

---
+++ 2019/js/main.js
@@ -0,0 +1,20 @@
+// Declare variables
+const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('#menu');
+const close = document.querySelector('.closebtn');
+
+// Add event listener
+menu.addEventListener("click", openNav);
+close.addEventListener("click", closeNav);
+
+// Open navigation
+function openNav() {
+  close.style.display = "block";
+  overlay.style.width = "80%";
+}
+
+// close navigation
+function closeNav() {
+  close.style.display = "none";
+  overlay.style.width = "0";
+}
