82c4a36cf9ada35f50a2f34ec05d8bc330f8cd11
Rafael Rodriguez <rodrigrv87@gmail.com>
2019-05-28 01:08:07 -0500

    Added mousewheel to homepage slider. Still working on a fix.

--- 2019/js/main.js
+++ 2019/js/main.js
@@ -1,3 +1,4 @@
+// Navigation
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector("#menu");
 const close = document.querySelector(".closebtn");
@@ -17,3 +18,14 @@ function closeNav() {
   close.style.display = "none";
   overlay.style.width = "0";
 }
+
+/* Owl Carousel */
+var owl = $('.owl-carousel');
+owl.on('mousewheel', '.owl-stage', function (e) {
+  e.preventDefault();
+  if (e.deltaY > 0) {
+    owl.trigger('next.owl');
+  } else {
+    owl.trigger('prev.owl');
+  }
+});
