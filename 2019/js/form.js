60fcf2a74713d83202d34264c894011466a89904
Rafael Rodriguez <rodrigrv87@gmail.com>
2019-06-02 23:52:12 -0500

    Finished validation for contact form

--- 2019/js/form.js
+++ 2019/js/form.js
@@ -1,101 +1,132 @@
-const submitColor = "#F47A5E";
+// Variables
+const submitGood = "#F47A5E";
+const submitBad = "#5A5A5A";
 const formName = document.querySelector(".name");
 const formEmail = document.querySelector(".email");
 const formMsg = document.querySelector(".message");
 const formSubmit = document.querySelector(".submit");
-const formSubmitValue = formSubmit.value;
+const form = document.getElementById("contact-form");
 
-formSubmit.addEventListener("click", formValidation);
+// Add event listeners
+formName.addEventListener("focusout", validateName);
+formEmail.addEventListener("focusout", validateEmail);
+formMsg.addEventListener("focusout", validateMessage);
 
-function formValidation(e) {
-  // Prevent submission
-  e.preventDefault();
+// Validators
+function validateName() {
+  formName.value.trim();
+  if (checkEmpty(formName)) {
+    isFormValid();
+    return;
+  }
+  // Checking for letters
+  if (!checkingName(formName)) {
+    isFormValid();
+    return;
+  }
 
-  let formNameValue = formName.value;
-  let formEmailValue = formEmail.value;
-  let formMsgValue = formMsg.value;
-  let validName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+  isFormValid();
+}
+
+function validateEmail() {
+  if (checkEmpty(formEmail)) {
+    isFormValid();
+    return;
+  }
+  // Checking validity
+  if (!checkingEmail(formEmail)) {
+    isFormValid();
+    return;
+  }
+
+  isFormValid();
+}
+
+function validateMessage() {
+  if (checkEmpty(formMsg)) {
+    isFormValid();
+    return;
+  }
+  // Checking for min of 2 characters
+  if (!checkingMessage(formMsg)) {
+    isFormValid();
+    return;
+  }
+
+  isFormValid();
+}
+
+function isFormValid() {
+  const allInputs = form.querySelectorAll('inputs, textarea');
+  let valid = true;
+
+  allInputs.forEach((input) => {
+    if (!input.classList.contains('valid')) {
+      valid = false;
+    }
+  });
+
+  toggleSubmit(valid);
+  return valid;
+}
+
+// Utility functions
+function checkEmpty(field) {
+  if (!field.value.trim().length) {
+    setInvalid(field, `${field.name} cannot be empty.`);
+    return true;
+  } else {
+    setValid(field);
+    return false;
+  }
+}
+
+function setInvalid(field, message) {
+  field.classList.remove('valid');
+  field.classList.add('invalid');
+  field.nextElementSibling.innerText = message;
+  return false;
+}
+
+function setValid(field) {
+  field.classList.remove('invalid');
+  field.classList.add('valid');
+  field.nextElementSibling.innerText = "";
+  return true;
+}
+
+function checkingName(field) {
+  let validName = /^[a-z ,.'-]+$/i;
+  if (validName.test(field.value)) {
+    return setValid(field);
+  } else {
+    return setInvalid(field, `${field.name} must only be letters.`);
+  }
+}
+
+function checkingEmail(field) {
   let validEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-
-  // Test if the name is empty or null
-  if (formNameValue == "" || formNameValue == null) {
-    formName.nextElementSibling.style.display = "block";
-    formName.nextElementSibling.innerText = "Please enter a name.";
-    formName.style.background = "#fbd3ce";
-    formName.style.marginBottom = "0";
-    formName.focus();
-
-    // Check if the value has a first and last name (really loose regex)
-  } else if (validName.test(formNameValue) == false) {
-    formName.nextElementSibling.style.display = "block";
-    formName.style.marginBottom = "0";
-    formName.style.background = "#fbd3ce";
-    formName.nextElementSibling.innerText =
-      "Please enter a first and last name.";
-    formName.focus();
-
-    // If all is good show indication for it
+  if (validEmail.test(field.value)) {
+    return setValid(field);
   } else {
-    formName.style.borderColor = "#71d996";
-    formName.style.marginBottom = "1rem";
-    formName.style.background = "none";
-    formName.nextElementSibling.style.display = "none";
+    return setInvalid(field, `${field.name} needs at least @ symbol and domain.`);
   }
+}
 
-  // Test is the email is empty or null
-  if (formEmailValue == "" || formEmailValue == null) {
-    formEmail.nextElementSibling.style.display = "block";
-    formEmail.nextElementSibling.innerText = "Please enter an email address.";
-    formEmail.style.background = "#fbd3ce";
-    formEmail.style.marginBottom = "0";
-    formEmail.focus();
-
-    // Check if the value is valid
-  } else if (validEmail.test(formEmailValue) == false) {
-    formEmail.nextElementSibling.style.display = "block";
-    formEmail.style.marginBottom = "0";
-    formEmail.style.background = "#fbd3ce";
-    formEmail.nextElementSibling.innerText =
-      "Please enter a valid email address.";
-    formEmail.focus();
-
-    // If all is good show indicator
+function checkingMessage(field) {
+  if (field.value.length >= 2) {
+    return setValid(field);
   } else {
-    formEmail.style.borderColor = "#71d996";
-    formEmail.style.marginBottom = "1rem";
-    formEmail.style.background = "none";
-    formEmail.nextElementSibling.style.display = "none";
+    return setInvalid(field, `${field.name} needs at least 2 characters.`);
   }
+}
 
-  // Test is the message is empty or null
-  if (formMsgValue == "" || formMsgValue == null) {
-    formMsg.nextElementSibling.style.display = "block";
-    formMsg.nextElementSibling.innerText = "Please enter an email address.";
-    formMsg.style.background = "#fbd3ce";
-    formMsg.style.marginBottom = "0";
-    formMsg.focus();
-
-    // If all is good, send the message
+function toggleSubmit(isValid) {
+  if (isValid) {
+    formSubmit.disabled = false;
+    formSubmit.style.background = submitGood;
   } else {
-    formMsg.style.borderColor = "#71d996";
-    formMsg.style.marginBottom = "1rem";
-    formMsg.style.background = "none";
-    formMsg.nextElementSibling.style.display = "none";
-  }
-
-  // Once message is typed, show submit button color
-  formMsg.addEventListener(
-    "keyup",
-    function() {
-      if (formMsgValue.length) {
-        formSubmit.style.background = submitColor;
-      }
-    },
-    false
-  );
-
-  // Make sure all data is filled in
-  if (formMsgValue && formEmailValue && formMsgValue != false) {
-    formSubmitValue.submit();
+    formSubmit.disabled = true;
+    formSubmit.style.background = submitBad;
   }
 }
