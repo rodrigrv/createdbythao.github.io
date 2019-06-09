35960b026f61362ef7f4636d6c7a869836f3c38b
Rafael Rodriguez <rodrigrv87@gmail.com>
2019-06-09 00:49:35 -0500

    Fixed safari nav bug showing all the time on mobile.

--- 2019/js/main.js
+++ 2019/js/main.js
@@ -11,6 +11,7 @@ close.addEventListener("click", closeNav);
 function openNav() {
   close.style.display = "block";
   overlay.style.width = "80%";
+  overlay.style.visibility = "visible";
   menu.style.visibility = "hidden";
 }
 
@@ -18,6 +19,7 @@ function openNav() {
 function closeNav() {
   close.style.display = "none";
   overlay.style.width = "0";
+  overlay.style.visibility = "hidden";
   menu.style.visibility = "visible";
 }
 
