5aa327558ff115e453d2e4fe696b88eb66720692
rodrigrv <rodrigrv87@gmail.com>
2024-07-24 00:19:50 -0500

    Updated more navigation styling.

--- 2024/js/main.js
+++ 2024/js/main.js
@@ -1,7 +1,11 @@
 const closeNav = document.querySelector('.close')
 const navigation = document.querySelector('nav')
+const navMenu = document.querySelector('.menu')
 
+navMenu.addEventListener('click', () => {
+  navigation.style.right = "0vw"
+})
 
 closeNav.addEventListener('click', () => {
-  navigation.style.right = '-65vw'
+  navigation.style.right = '-90vw'
 })
