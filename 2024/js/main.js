9524ada97f0082193f5b8db08fef95f7622ee3df
rodrigrv <rodrigrv87@gmail.com>
2024-07-25 01:27:58 -0500

    Finished most of the mobile nav styling.

--- 2024/js/main.js
+++ 2024/js/main.js
@@ -1,11 +1,16 @@
 const closeNav = document.querySelector('.close')
 const navigation = document.querySelector('nav')
 const navMenu = document.querySelector('.menu')
+const pageBody = document.querySelector('body')
 
 navMenu.addEventListener('click', () => {
   navigation.style.right = "0vw"
+  pageBody.classList.add("nav-open")
+  pageBody.classList.remove("nav-close")
 })
 
 closeNav.addEventListener('click', () => {
   navigation.style.right = '-90vw'
+  pageBody.classList.add("nav-close")
+  pageBody.classList.remove("nav-open")
 })
