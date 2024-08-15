b9ed7a86209ddc26c3c9e31e1f124bdb7e9e2eab
rodrigrv <rodrigrv87@gmail.com>
2024-08-15 01:52:45 -0500

    Finally fixed large homepage layout.

--- 2024/js/main.js
+++ 2024/js/main.js
@@ -2,15 +2,23 @@ const closeNav = document.querySelector('.close')
 const navigation = document.querySelector('nav')
 const navMenu = document.querySelector('.menu')
 const pageBody = document.querySelector('body')
+const pageLinks = document.querySelectorAll('.nav-container a')
 
-navMenu.addEventListener('click', () => {
-  navigation.style.right = "0vw"
-  pageBody.classList.add("nav-open")
-  pageBody.classList.remove("nav-close")
-})
+function openNavigation() {
+  navMenu.addEventListener('click', () => {
+    navigation.style.right = "0vw"
+    pageBody.classList.add("nav-open")
+    pageBody.classList.remove("nav-close")
+  })
+}
 
-closeNav.addEventListener('click', () => {
-  navigation.style.right = '-90vw'
-  pageBody.classList.add("nav-close")
-  pageBody.classList.remove("nav-open")
-})
+function closeNavigation() {
+  closeNav.addEventListener('click', () => {
+    navigation.style.right = '-90vw'
+    pageBody.classList.add("nav-close")
+    pageBody.classList.remove("nav-open")
+  })
+}
+
+openNavigation();
+closeNavigation();
