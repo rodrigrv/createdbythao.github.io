af328e6298e7866ab1ccf718ee4d482ba0c75cbb
rodrigrv <rodrigrv87@gmail.com>
2024-08-17 16:22:46 -0500

    Added functionality for closing nav on homepage after tapping a main category.

--- 2024/js/main.js
+++ 2024/js/main.js
@@ -1,43 +1,41 @@
-const closeNav = document.querySelector('.close')
-const navigation = document.querySelector('nav')
-const navMenu = document.querySelector('.menu')
-const pageBody = document.querySelector('body')
-const pageLinks = document.querySelectorAll('.nav-container a')
-const urlBookmark = window.location.href.split('/')[8]
+window.addEventListener("DOMContentLoaded", function (event) {
+  const closeNav = document.querySelector('.close')
+  const navigation = document.querySelector('nav')
+  const navMenu = document.querySelector('.menu')
+  const pageBody = document.querySelector('body')
+  const pageLinks = document.querySelectorAll('.nav-container a')
+  const urlBookmark = window.location.hash
 
-navMenu.addEventListener('click', () => {
-  openNavigation()
+  navMenu.addEventListener('click', () => {
+    openNavigation()
+  })
+
+  closeNav.addEventListener('click', () => {
+    closeNavigation()
+  })
+
+  function openNavigation() {
+    navigation.style.right = "0vw"
+    pageBody.classList.add("nav-open")
+    pageBody.classList.remove("nav-close")
+  }
+
+  function closeNavigation() {
+    navigation.style.right = '-90vw'
+    pageBody.classList.add("nav-close")
+    pageBody.classList.remove("nav-open")
+  }
+
+  if (screen.width <= 768) {
+    pageLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        if (link.hash == "#projects" || "#about" || "#contact") {
+          setTimeout(() => {
+            closeNavigation()
+          }, 750)
+        }
+      })
+    })
+  }
 })
-
-closeNav.addEventListener('click', () => {
-  closeNavigation()
-})
-
-function openNavigation() {
-  navigation.style.right = "0vw"
-  pageBody.classList.add("nav-open")
-  pageBody.classList.remove("nav-close")
-}
-
-function closeNavigation() {
-  navigation.style.right = '-90vw'
-  pageBody.classList.add("nav-close")
-  pageBody.classList.remove("nav-open")
-}
-
-// pageLinks.forEach((link) => {
-//   console.log(link.hash)
-//   if(link.hash == urlBookmark) {
-//     console.log('this matches, please close')
-//     closeNavigation();
-//   }
-// })
-
-// if (urlBookmark == "#projects") {
-//   closeNavigation()
-// } else if (urlBookmark == "#about") {
-//   closeNavigation()
-// } else if (urlBookmark == "#contact") {
-//   closeNavigation()
-// }
 
