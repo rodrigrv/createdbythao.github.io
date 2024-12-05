6b2929c05834ac17ebecee6da03bcb380f034f8c
rodrigrv <rodrigrv87@gmail.com>
2024-12-04 22:14:33 -0600

    Changed image path from project name to production name.

--- js/nav-footer.js
+++ js/nav-footer.js
@@ -3,7 +3,7 @@ const aside =
   <div class="nav-container">
     <div class="logo-wrapper">
       <div class="img-container">
-        <a href="/2024/index.html">
+        <a href="/index.html">
           <img src="https://res.cloudinary.com/thaos-images/image/upload/v1732400447/th-logo-orange_hdhkoy.svg" alt="Created By Thao" />
         </a>
       </div>
@@ -12,15 +12,15 @@ const aside =
     <nav>
       <button class="close">Close<span>&times;</span></button>
       <ul class="main-nav">
-        <li><a href="/2024/index.html#projects">Projects</a></li>
+        <li><a href="/index.html#projects">Projects</a></li>
         <ul class="subnav">
-          <li><a href="/2024/projects/destination-success.html">Destination Success</a></li>
-          <li><a href="/2024/projects/texas-extra-credit.html">Texas Extra Credit</a></li>
-          <li><a href="/2024/projects/annual-report.html">Annual Report</a></li>
-          <li><a href="/2024/projects/gigis-party-decor">Gigi's Party Decor</a></li>
+          <li><a href="/projects/destination-success.html">Destination Success</a></li>
+          <li><a href="/projects/texas-extra-credit.html">Texas Extra Credit</a></li>
+          <li><a href="/projects/annual-report.html">Annual Report</a></li>
+          <li><a href="/projects/gigis-party-decor">Gigi's Party Decor</a></li>
         </ul>
-        <li><a href="/2024/index.html#about">About</a></li>
-        <li><a href="/2024/index.html#contact">Contact</a></li>
+        <li><a href="/index.html#about">About</a></li>
+        <li><a href="/index.html#contact">Contact</a></li>
       </ul>
     </nav>
   </div>
